9df460d1b972f07004bae70f08db180bcab3f072
BayalAI <bayaldioum@gmail.com>
2026-01-30 10:44:49 +0100

    Delete guide_complet_dbt directory

--- guide_complet_dbt/src/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// Extend Array prototype for utility methods
-declare global {
-  interface Array<T> {
-    takeWhile(callback: (value: T, index: number, array: T[]) => boolean): T[]
-  }
-}
-
-export {}
